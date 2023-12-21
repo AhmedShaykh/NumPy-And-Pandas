0367f568b29ee9ec5bf69295926fd3686077e870
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-12-21 17:42:01 +0500

    Final Commit

--- pandas3.py
+++ pandas3.py
@@ -4,7 +4,6 @@ from pandera.typing import Series;
 from datetime import datetime;
 
 class StudentSchema(pa.SchemaModel):
-    
     Roll_No: Series[int] = pa.Field(ge=1);
     Name: Series[str] = pa.Field(nullable=False);
     Father_Name: Series[str] = pa.Field(nullable=False);
